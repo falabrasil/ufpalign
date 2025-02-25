76d3fbf688ee2ee3dd0042bb969c23a3aeba636b
Cassio T. Batista <cassiotbatista@gmail.com>
2025-02-25 05:09:28 +0100

    [utils] update data gz pkg fetched from gdrive with syllphones dict (#19)

--- utils/check_dependencies.sh
+++ utils/check_dependencies.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 #
 # author: jan 2023
-# cassio batista - https://cassota.gitlab.io
+# Cassio T Batista - https://cassiotbatista.github.io
+# last update: feb 2025
 
 ok=true
 for f in sudo tar java wget curl python3 gdown ; do
   ! type -f $f > /dev/null 2>&1 && ok=false && \
     echo "$0: $f not installed"
 done
-for p in unidecode ; do
+for p in icu pandas textgrid unidecode ; do
   ! python3 -c "import $p" 2>/dev/null && ok=false && \
     echo "$0: please install $p python package"
 done
--- utils/download_model.sh
+++ utils/download_model.sh
@@ -3,12 +3,13 @@
 # downloads acoustic model files in tar.gz format from Google Drive
 # 
 # author: apr 2021
-# cassio batista - https://cassota.gitlab.io
-# last update: jan 2023
+# Cassio T Batista - https://cassiotbatista.github.io
+# last update: feb 2025
 
 # https://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
 declare -A files=(
-  ["data.tar.gz"]="1677Chb1JDCttzBwyRm5_P-3ELoj6VaYH"
+  #["data.tar.gz"]="1677Chb1JDCttzBwyRm5_P-3ELoj6VaYH"
+  ["data.tar.gz"]="1LV079CNWO_rq8bPKUSOYXCeF-f1SheZS"
   ["mono.tar.gz"]="1WlOMnb0P_VNVwHs1iII78aMNjND6finP"
   ["tri1b.tar.gz"]="1ie3NXKKfmk4bPHcnhIW4v0zelXmcjKIl"
   ["tri2b.tar.gz"]="11rR2UtJahj7KHbW0hRfm55HFNm6pvCcP"
@@ -17,7 +18,6 @@ declare -A files=(
   ["ie.tar.gz"]="19VyLt6GpPJQmPdEwbsGuwiufdEUS5E_f"
 )
 
-# main
 if [ $# -ne 2 ] ; then
   echo "usage: $0 <tag> <model-dir>"
   echo "  <tag> is the resource tag"
@@ -32,7 +32,8 @@ mkdir -p $dir || exit 1
 filename=$tag.tar.gz
 filehash=${files[$filename]}
 
-[ ! -f $dir/$filename ] && gdown -O $dir/$filename "$filehash" || \
+[ ! -f $dir/$filename ] && \
+  gdown -O $dir/$filename "$filehash" || \
   echo "$0: file '$dir/$filename' exists. skipping download"
 tar xf $dir/$filename -C $dir || exit 1
 
